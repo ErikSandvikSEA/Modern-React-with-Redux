1b199f445b5d0fb0859e8e264c36f4abb593ebf3
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-09-24 12:05:37 -0700

    starter complete

--- app-modern-react-with-redux/src/index.js
+++ app-modern-react-with-redux/src/index.js
@@ -4,11 +4,15 @@ import ReactDOM from 'react-dom'
 
 //Create a react component
 const App = function() {
+    const buttonText = 'Click Me!'
+    const labelText = 'Enter name: '
     return (
         <div>
-            <label>Enter name:</label>
+            <label className='label' htmlFor='name'>
+                {labelText}
+            </label>
             <input id="name" type="text"/>
-            <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+            <button style={{backgroundColor: 'blue', color: 'white', borderRadius: '5px'}}>{buttonText}</button>
         </div>
     )
 }
