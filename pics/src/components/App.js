dcccdf2d8113b10f596bd13e9a774978e07f4e64
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-10-15 16:19:50 -0700

    bringing in axios and adding get request

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,5 +1,6 @@
 // from packages 
 import React, { Component } from 'react'
+import axios from 'axios'
 
 // from files 
 import { SearchBar } from '../components'
@@ -7,7 +8,13 @@ import '../styles/App.css'
 
 class App extends Component {
     onSearchSubmit(term){
-        console.log(term)
+        axios
+            .get(`https://api.unsplash.com/search/photos`, {
+                params: { query: term },
+                headers: {
+                    Authorization: `Client-ID NCOYwbvxs_HrYSjK95BNoMPS5EyxbMyRsACAXWmO6DY`
+                }
+            }) 
     }
 
 
