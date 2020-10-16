bd0b03e40d45a7b6d5b224e805da096e32907d03
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-10-15 17:18:27 -0700

    images rendering with keys

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -3,9 +3,16 @@ import React from 'react'
 function ImageList(props) {
     const { images } = props
 
+    const imagesToRender = images.map(image =>{
+        const { urls, id, alt_description } = image
+        return (
+        <img key={id} src={urls.regular} alt={alt_description}/>
+        )
+    })
+
     return (
         <div>
-            ImageList
+            {imagesToRender}
         </div>
     )
 }
