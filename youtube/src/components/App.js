1a4bd5c93f530666afb8d624961bc551c021c525
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-10-17 14:42:22 -0700

    adding components

---
+++ youtube/src/components/App.js
@@ -0,0 +1,17 @@
+// from packages
+import React, { Component } from 'react'
+
+// from files
+import { SearchBar } from '../components'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ youtube/src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+                Yo
+            </div>
+        )
+    }
+}
+
+export default SearchBar
