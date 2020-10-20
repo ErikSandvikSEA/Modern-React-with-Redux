e9413daa2b269f3c2125b8c79b8c0400de2a5347
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-10-20 16:09:45 -0700

    bringing in wikipedia API for search queries

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,15 +1,65 @@
+// from packages 
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+// from files 
 
 function Search() {
-    const [term, setTerm] = useState(``)
+    const [term, setTerm] = useState(`programming`)
+    const [results, setResults] = useState([])
 
     const changeHandler = e => {
         setTerm(e.target.value)
     }
 
+    console.log(results)
+
     useEffect(()=> {
-        console.log('yo')
-    }, [])
+        const search = async () => {
+            const response = await axios.get(`https://en.wikipedia.org/w/api.php`, {
+                params: {
+                    action: `query`,
+                    list: `search`,
+                    origin: `*`,
+                    format: `json`,
+                    srsearch: term
+                }
+            })
+
+            setResults(response.data.query.search)
+        }
+        const timeoutId = setTimeout(() => {
+            if(term){
+                search()
+            }
+        }, 500)
+    }, [term])
+
+    const renderedResults = results.map(result => {
+        return (
+            <div 
+                className="item"
+                key={result.pageid}
+            >
+                <div className="right floated content">
+                    <a 
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                            Go
+                        </a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+                </div>
+            </div>
+        )
+    })
 
     return (
         <div>
@@ -23,7 +73,9 @@ function Search() {
                     />
                 </div>
             </div>
-            Search
+            <div className="ui celled list">
+                {renderedResults}
+            </div>
         </div>
     )
 }
