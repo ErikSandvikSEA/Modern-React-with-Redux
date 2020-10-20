8233e8c0deb42db25fdab0f74278c74e51640752
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-10-19 19:46:07 -0700

    adding widgets

---
+++ widgets/src/App.js
@@ -0,0 +1,15 @@
+// from packages 
+import React from 'react'
+
+// from files 
+import { Accordion } from './components'
+
+function App() {
+    return (
+        <div>
+            <Accordion />
+        </div>
+    )
+}
+
+export default App
