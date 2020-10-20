23513a4d16c32002417a6ca96f0d4623fa3cba07
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-10-19 19:58:12 -0700

    adding click handler to accordion

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -4,10 +4,25 @@ import React from 'react'
 // from files 
 import { Accordion } from './components'
 
+const items = [
+    {
+        title: `What is React?`,
+        content: `React is a front end javascript framework`
+    },
+    {
+        title: `Why use React?`,
+        content: `React is a favorite JS library`
+    },
+    {
+        title: `Ok but why?`,
+        content: `Because`
+    }
+]
+
 function App() {
     return (
         <div>
-            <Accordion />
+            <Accordion items={items}/>
         </div>
     )
 }
--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
-function Accordion() {
+function Accordion(props) {
+    const {
+        items
+    } = props
+
+    const onTitleClick = (index) => {
+        console.log(`Title clicked: `, index)
+    }
+
+    const renderedItems = items.map((item, idx) => {
+        return (
+            <React.Fragment key={item.title}>
+                <div 
+                    className="title active"
+                    onClick={() => onTitleClick(idx)}
+                >
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+                <div className="content active">
+                    <p>{item.content}</p>
+                </div>
+            </React.Fragment>
+        )
+    })
+
     return (
-        <div>
-            <h1>Accordion</h1>
+        <div className="ui styled accordion">
+            {renderedItems}
         </div>
     )
 }
